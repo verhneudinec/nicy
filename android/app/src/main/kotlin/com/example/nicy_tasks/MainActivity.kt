e186da79035df7050a874f73d8db7612cc15939a
Igor` <webarts@yandex.ru>
2021-10-26 15:18:45 +0800

    First commit

---
+++ android/app/src/main/kotlin/com/example/nicy_tasks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nicy_tasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feature/domain/model/task.dart
@@ -0,0 +1,13 @@
+class Task {
+  final String title;
+  final String content;
+  final DateTime created;
+  final DateTime dueDate;
+
+  Task({
+    required this.title,
+    required this.content,
+    required this.created,
+    required this.dueDate,
+  });
+}
