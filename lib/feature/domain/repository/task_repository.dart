eed79b49bd0eefa43cfa756e01d23ada7a5dded6
Igor` <webarts@yandex.ru>
2021-10-28 22:17:31 +0800

    Попытка #2

--- lib/feature/domain/repository/task_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:meta/meta.dart';
-import 'package:nicy_tasks/feature/domain/model/task.dart';
-
-abstract class TaskRepository {
-  Future<List<Task>> getAllTasks();
-
-  Future<Task> deleteTaskById(int id);
-}
